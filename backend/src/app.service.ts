9163dd24e1faa2f2f05779d6979a47e0d6bf7469
Ratchanon Pawanja <68798300+march250602@users.noreply.github.com>
2025-09-22 13:34:33 +0700

    Update app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -15,12 +15,15 @@ export class AppService  {
   constructor(@Inject('HIGH_SCORE_SERVICE') private client: ClientProxy) {}
 
   updateScore(playerScore: number) {
-     
+     if (typeof playerScore !== 'number' || isNaN(playerScore)) {
+    return { updated: false };
+  }
     if (playerScore > this.highScore) {
       this.highScore = playerScore;
       this.client.emit(process.env.QUEUE_NAME , { highScore: this.highScore });
+     return { updated: true, highScore: this.highScore };
     }
-    return this.highScore;
+    return { updated: false, highScore: this.highScore };
   }
 
   // ================================
